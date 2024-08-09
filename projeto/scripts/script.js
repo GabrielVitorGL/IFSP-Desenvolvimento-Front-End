a12086d2c47563a70457b6f1a1bcf9114d242102
Gabriel Vitor <gabrielvitorcom@hotmail.com>
2024-08-08 22:42:59 -0300

    adding project files from laptop

---
+++ projeto/scripts/script.js
@@ -0,0 +1,16 @@
+
+window.onscroll = function() {myFunction()};
+
+
+var header = document.querySelector("header");
+
+
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.scrollY > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+} 
